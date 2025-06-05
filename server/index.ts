2d46267a63c44b255f5ca992f34321175f5cb19b
Nitish Kumar - RE <Nitish.9.Kumar@coforge.com>
2025-06-05 10:05:10 +0530

    server config changes success

--- server/index.ts
+++ server/index.ts
@@ -55,8 +55,9 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const port = 5000;
-  server.listen(port, () => {
-    log(`serving on port ${port}`);
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+  
 })();
